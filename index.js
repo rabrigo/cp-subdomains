e84a3f8b8bd2630686e54e569730e786ef266507
the rawness <ronnelabrigo@gmail.com>
2023-10-13 18:02:02 -0700

    solved first case

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-function sortInput(input) {
-    console.log(input);
-    const visitsDomains = input[0].split(" ");
-    const visitCount = visitsDomains[0];
-    const subdomains = visitsDomains[1].split(".");
-    console.log(visitCount);
-    console.log(subdomains);
-    countVisits(visitCount, subdomains)
-}
-
-function countVisits(count, domains) {
-    let output = [];
-    for (i = domains.length - 1; i >= 0; i--) {
-        if (i >= domains.length - 1) {
-            output.push(`${count} ${domains[i]}`);
-        } else {
-            output.push(`${count} ${domains[i]}`)
-        }
+function processArray(array) {
+    for (i = 0; i < array.length; i++) {
+        console.log(i);
+        splitString(array, i);
     }
-    console.log(output);
 }
 
-cpdomains = ["9001 discuss.leetcode.com"]
-sortInput(cpdomains);
+function splitString(input, index) {
+    const j = input[index].split(" ");
+    const count = parseInt(j[0]);
+    const domains = j[1].split(".");
+    console.log('here is the count: ' + count);
+    console.log('here is the domain: ' + domains);
+    countVisits(count, domains);
+}
+
+function countVisits(count, subdomains) {
+    for (i = 0; i < subdomains.length; i++) {
+        let temp = ``;
+        for (j = i; j < subdomains.length; j++) {
+            if (j == subdomains.length - 1) {
+                temp+= `${subdomains[j]}`;
+                // checkDomain(count, temp);
+            } else {
+                temp+= `${subdomains[j]}.`
+                // checkDomain(count, temp);
+            }
+        }
+        // console.log('this is what temp looks like: ' + temp);
+        checkDomain(count, temp);
+    }
+}
+
+function checkDomain(visitCount, subdomainsList) {
+    // console.log('here is the count ' + count)
+    // console.log('here is the domain you are checking ' + subdomains)
+    if (domainPairs[subdomainsList]) {
+        domainPairs[subdomainsList] += visitCount;
+    } else {
+        domainPairs[subdomainsList] = visitCount;
+    }
+}
+
+const domainPairs = {};
+cpdomains = ["9000 discuss.leetcode.com", "1 discuss.leetcode.com", "2 email.com"]
+processArray(cpdomains);
+console.log(domainPairs);
